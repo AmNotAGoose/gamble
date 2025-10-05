f07c0e553999bfc1836c43583f7ebd665b1808ee
AmNotAGoose <57881736+AmNotAGoose@users.noreply.github.com>
2025-10-04 21:34:28 -0400

    Basic scene test of one spinning item with proper bounds
    it wokrs !!

--- Assets/Scripts/SlotsGame/SlotColumn.cs
+++ Assets/Scripts/SlotsGame/SlotColumn.cs
@@ -9,26 +9,47 @@ public class SlotColumn : MonoBehaviour
     public float curVelocity;
     public float maxVelocity;
 
+    public float maxY = 3;
+    public float minY = -3;
+
     public bool isStopped = true;
 
     // Items
     public List<SlotItem> slotItems;
 
+    private void Start()
+    {
+        StartCoroutine(StartSpinning());
+    }
+
     private void Update()
     {
+        foreach (SlotItem item in slotItems)
+        {
+            item.transform.position = new Vector2(item.transform.position.x, item.transform.position.y - curVelocity * Time.deltaTime);
         
+            if (item.transform.position.y < minY)
+            {
+                item.transform.position = new Vector2(item.transform.position.x, maxY);
+            }
+        }
     }
+
 
     public IEnumerator StartSpinning()
     {
+        isStopped = false;
+
         while (!isStopped)
         {
-            curVelocity = Mathf.Min(curVelocity + acceleration * Time.deltaTime, maxVelocity);
+            curVelocity = Mathf.Min(curVelocity + acceleration * Time.deltaTime, maxVelocity); // read it carefully
+            yield return null;
         }
         curVelocity = 0; 
-        yield return null;
+        yield break;
     }
 
+    // i am NOT doing physics in my spare time, so i wont be implementing rigged / predetermined games
     public void StopSpinning()
     {
         isStopped = true;
