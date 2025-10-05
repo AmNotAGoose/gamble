ad666c8ae117a2a920f57654f65e05fa799a5620
AmNotAGoose <57881736+AmNotAGoose@users.noreply.github.com>
2025-10-05 10:51:38 -0400

    subscriber system for coins

--- Assets/Scripts/PlayerDataManager.cs
+++ Assets/Scripts/PlayerDataManager.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Runtime.CompilerServices;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerDataManager : MonoBehaviour
@@ -5,14 +8,42 @@ public class PlayerDataManager : MonoBehaviour
     public static PlayerDataManager Instance;
     public int coins = 500;
 
+    public List<Action<int>> coinChangeSubscribers = new List<Action<int>>();
+
     private void Awake()
     {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
         Instance = this;
         DontDestroyOnLoad(Instance);
+    }
+    private void OnLevelWasLoaded(int level)
+    {
+        InformSubscribers();
+    }
+
+    public void InformSubscribers()
+    {
+        foreach (Action<int> subscriber in coinChangeSubscribers)
+        {
+            subscriber(coins);
+        }
     }
 
     public void AddCoins(int numberOfCoins)
     {
         coins += numberOfCoins;
+        foreach (Action<int> subscriber in coinChangeSubscribers)
+        {
+            subscriber(coins);
+        }
+    }
+
+    public void SubscribeToCoinChanges(Action<int> action)
+    {
+        coinChangeSubscribers.Add(action);
     }
 }
--- Assets/Scripts/SlotsGame/SlotMachine.cs
+++ Assets/Scripts/SlotsGame/SlotMachine.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,11 +8,16 @@ public class SlotMachine : MonoBehaviour
     public List<SlotColumn> columns = new List<SlotColumn>();
     public List<List<GameObject>> positions = new List<List<GameObject>>();
 
+    bool isAutoSpinning = false;
+    bool isSpinning = false;
+
     private void Start()
     {
-        Spin();
+        StartCoroutine(AutoSpin(GetSampleValues));
+    }
 
-
+    List<List<int>> GetSampleValues()
+    {
         List<List<int>> sampleValues = new List<List<int>>()
         {
             new List<int>() { 0, 1, 2, 3 },
@@ -21,28 +27,39 @@ public class SlotMachine : MonoBehaviour
             new List<int>() { 0, 1, 2, 3 },
         };
 
-        StartCoroutine(Stop(sampleValues));
+        return sampleValues;
     }
 
-    public void AutoSpin()
+    public IEnumerator AutoSpin(Func<List<List<int>>> func)
     {
+        while (true)
+        {
+            Spin();
+            yield return new WaitForSeconds(3f);
+            StartCoroutine(Stop(func));
+            yield return new WaitUntil(() => !isSpinning);
 
+            if (isAutoSpinning) yield break;
+        }
     }
 
     public void Spin()
     {
         foreach (SlotColumn col in columns)
         {
-            col.StartSpinning();
+            StartCoroutine(col.StartSpinning());
         }
+        isSpinning = true;
     }
 
-    public IEnumerator Stop(List<List<int>> values)
+    public IEnumerator Stop(Func<List<List<int>>> func) 
     {
+        List<List<int>> values = func();
         for (int i = 0; i < columns.Count; i++)
         {
             StartCoroutine(columns[i].StopSpinning(values[i]));
             yield return new WaitForSeconds(0.5f);
         }
+        isSpinning = false;
     }
 }
